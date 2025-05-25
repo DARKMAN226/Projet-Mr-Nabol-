58a1fbcbfd3e1a0ef28c5e40c3ae626faa97263b
Darkdevs <darkmanterrible@gmail.com>
2025-05-25 17:08:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -245,4 +245,5 @@ def initdb_command():
     print('✅ Base de données initialisée.')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+
